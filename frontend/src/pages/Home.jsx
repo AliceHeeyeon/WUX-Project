8cf660776e6a8be23d3f29f9abad1b0d70872199
russiankiwi <mashadasasha@mail.ru>
2023-08-24 10:24:11 +1200

    starting home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
 const Home = () => {
+  const [projects, setProjects] = useState(null)
+
+  useEffect(() => {
+       
+    const fetchProjects = async () => {
+      const response = await axios.get('http://localhost:4000/api/projects')
+    }
+
+    if (response.status === 200) {
+      setProjects(response.data)
+  }
+
+    fetchProjects()
+
+  }, [])
+
+
   return (
-    <div>
-      
+    <div className="home">
+        <div className="projects">
+           
+            {projects && projects.map((project) => {
+                return (
+                    <>
+                        <p key={project._id}>{project.title}</p>
+                    </>
+                )
+            })}
+        </div>
     </div>
   )
 }
