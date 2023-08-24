50ea349c60b96b6d6381b7e9761477fc3f14e864
Yassoori <yasserasaeed@gmail.com>
2023-08-25 10:47:52 +1200

    signup page done - but dont forget that the hook doesnt actually exist yet

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,18 +1,47 @@
-import React from 'react'
+import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // bring in signup function, loading state, error from our hook:
+  const { signup, error, isLoading } = useSignup();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await signup(email, password);
+  };
+
   return (
-    <div className='signup'>
-      <h3>Sign Up</h3>
-      <label>Username</label>
-      <input></input>
-      <label>Password</label>
-      <input></input>
+    <div className="signup">
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <h3>Sign Up</h3>
+        <label className="username-label">Username</label>
+        <input
+          type="username"
+          placeholder="Username"
+          className="username-input"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+        <label className="password-label">Password</label>
+        <input
+          type="password"
+          placeholder="Password"
+          className="password-input"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
 
-      <p>Already registered? <span>log In</span></p>
-      <button>Sign Up</button>
+        <p>
+          Already registered? <span>log In</span>
+        </p>
+        <button disabled={isLoading}>Sign Up</button>
+        {error && <div className="error">{error}</div>}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
