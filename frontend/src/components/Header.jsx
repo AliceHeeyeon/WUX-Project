05cdb81a3444b5a27dbfca4917bbc382455069a9
Yassoori <yasserasaeed@gmail.com>
2023-08-28 09:37:34 +1200

    fixed signup, accidently wrote email insusername in a few places

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,35 +1,35 @@
-import { useAuthContext } from "../hooks/useAuthContext"
-import { useLogout } from "../hooks/useLogout"
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
+import { Link } from "react-router-dom";
 
-const Header = ({onLoginClick}) => {
+const Header = ({ onLoginClick }) => {
   //Bring user through useAuthContext
-  const {user} = useAuthContext()
-  const {logout} = useLogout()
+  const { user } = useAuthContext();
+  const { logout } = useLogout();
 
   const handleLogout = () => {
-    logout()
-  }
+    logout();
+  };
 
   return (
     <div>
       <h1>WUX PROJECT</h1>
       <nav>
-
         {user && (
           <div>
             <span>Welcome {user.username}</span>
             <button onClick={handleLogout}>Logout</button>
-          </div>)}
+          </div>
+        )}
         {!user && (
           <div>
             <button onClick={onLoginClick}>Login</button>
-            <button>Sign Up</button>
+            <Link to="/signup">Sign Up</Link>
           </div>
         )}
-        
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
