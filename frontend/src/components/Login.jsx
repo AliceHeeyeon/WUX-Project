8a8b2f18b1cf0828eca7c0824b664ca66b28737d
Ryan Gudsell <ryangudsell@gmail.com>
2023-08-28 10:29:48 +1200

    Sass header and login modal

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -37,19 +37,23 @@ const Login = ({ onClose }) => {
 
         <h3>Log In</h3>
 
+        <div>
         <label>Username</label>
         <input 
             type='text'
             onChange={(e) => setUsername(e.target.value)}
             value={username}
         />
+        </div>
 
+        <div>
         <label>Password</label>
         <input 
             type='password'
             onChange={(e) => setPassword(e.target.value)}
             value={password}
         />
+        </div>
 
         <p>Not registered? <span>Sign Up</span></p>
         <button disabled={isLoading}>Log In</button>
